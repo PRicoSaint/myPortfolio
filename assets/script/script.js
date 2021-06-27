b3ea6cfa13c2927b2d8d677ecc3e136416f253dc
PRicoSaint <santosesteban09@gmail.com>
2021-06-27 18:43:33 -0500

    I have added stills to each project. When hovered over it will animate using the gif of the site. I have changed some of the colors and added fonts from googlefonts. Still need a bit of work but its getting there.

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+var examples = document.querySelector("#examples");
+
+var imageSwap = function () {
+    var $this = $(this);
+    var newSource = $this.data('swap');
+    $this.data('swap', $this.attr('src'));
+    $this.attr('src', newSource);
+}
+
+$(function () {
+  $('img.playgif').hover(imageSwap, imageSwap);
+});
+
